6744beb4b9100f5535545bf62e8194bfb7c29a37
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-09-14 19:43:56 -0700

    Basic Play/Stop Functionality First Step

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from gpiozero import LED, Button
 from signal import pause
 
@@ -15,16 +17,43 @@ class LedWithState:
         self.state = not self.state
 
 
-class LedWithButton:
+class AudioPlayer:
+    def __init__(self):
+        self.audio_file = '~/sample_audio_small.mp3'
+        self.process = None
+        self.state = False
+
+    def _play(self):
+        # "omxplayer ./sample_audio_small.mp3 -o local --pos 00:00:25"
+        cmd = 'omxplayer -o local {file}'.format(file=self.audio_file)
+        self.process = subprocess.Popen(cmd, shell=True)
+        print('process id: {}'.format(self.process.pid))
+
+    def _stop(self):
+        cmd = 'killall omxplayer.bin'
+        os.system(cmd)
+        print('must be terminated with this: {}'.format(cmd))
+
+    def toggle(self):
+        if self.state:
+            self._stop()
+        else:
+            self._play()
+        self.state = not self.state
+
+
+class Environment:
     def __init__(self, led_id, button_id):
         self.led = LedWithState(led_id)
         self.button = Button(button_id)
+        self.player = AudioPlayer()
         self.button.when_activated = self._btn_action
 
     def _btn_action(self):
+        self.player.toggle()
         self.led.toggle()
 
 
-led = LedWithButton(17, 2)
+led = Environment(17, 2)
 
 pause()
