cbe693ad60813524d0c83dae8b5d9fa1036ffcd0
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2019-02-22 16:16:10 -0800

    Add Datetime to Logger and Make Logfile Hidden File

--- logger.py
+++ logger.py
@@ -1,6 +1,9 @@
 import logging
 
-logging.basicConfig(filename='../caressa.log', level=logging.DEBUG)
+logging.basicConfig(filename='../.caressa.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s > %(levelname)s:%(name)s: %(message)s:%(message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S %p (%Z)')
 
 
 def get_logger():
